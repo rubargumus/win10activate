2c5ef58a1a14ebcbc487f18ee0c989290fa8c0f1
Rubar Gümüş <61204308+rubargumus@users.noreply.github.com>
2021-02-03 12:30:21 +0300

    Add files via upload

---
+++ win10.bat
@@ -0,0 +1,10 @@
+echo NUKE WÝN10 ACTÝVATE
+echo NUKE WÝN10 ACTÝVATE
+echo NUKE WÝN10 ACTÝVATE
+cls
+echo NUKE WÝN10 ACTÝVATE
+
+
+cscript slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+cscript slmgr.vbs /skms kms.lotro.cc
+cscript slmgr.vbs /ato
